75d09ea17ca86ea493cc2b8630ee331f2785ddbe
Hristo <generatorfox@gmail.com>
2026-05-24 19:29:28 +0300

    add update and delete products logic

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -30,3 +30,66 @@ export const createProduct = async (req: Request, res: Response) => {
     data: product,
   });
 };
+
+export const updateProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name, price } = req.body;
+
+  if (!name && !price) {
+    res.status(400).json({
+      success: false,
+      message: "Provide at least a name or price to update",
+    });
+    return;
+  }
+
+  const currentProduct = await prisma.product.findUnique({
+    where: { id: Number(id) },
+  });
+
+  if (!currentProduct) {
+    res.status(400).json({
+      success: false,
+      message: "cannot find product",
+    });
+    return;
+  }
+
+  if (name || price) {
+    const product = await prisma.product.update({
+      where: { id: Number(id) },
+      data: { name, price },
+    });
+
+    res.status(200).json({
+      success: true,
+      data: product,
+    });
+  }
+};
+
+export const deleteProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const currentProduct = await prisma.product.findUnique({
+    where: { id: Number(id) },
+  });
+
+  if (!currentProduct) {
+    res.status(404).json({
+      success: false,
+      message: "not existing product",
+    });
+    return;
+  }
+
+  const product = await prisma.product.delete({
+    where: { id: Number(id) },
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "product deleted",
+    data: product,
+  });
+};
--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { getProducts, createProduct } from "../controllers/products.controller";
+import {
+  getProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/products.controller";
 import { verifyToken } from "../middleware/auth.middleware";
 const router = Router();
 
 router.get("/", getProducts);
 router.post("/", verifyToken, createProduct);
-
+router.put("/:id", verifyToken, updateProduct);
+router.delete("/:id", verifyToken, deleteProduct);
 export default router;

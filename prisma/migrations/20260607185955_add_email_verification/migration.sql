ec13ef610cf47707229c76307b276a532b1648de
Hristo <generatorfox@gmail.com>
2026-06-07 22:05:30 +0300

    update user scheme

---
+++ prisma/migrations/20260607185955_add_email_verification/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[verificationCode]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `verificationExpiresAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "verificationCode" TEXT,
+ADD COLUMN     "verificationExpiresAt" TIMESTAMP(3) NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_verificationCode_key" ON "User"("verificationCode");
